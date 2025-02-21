00e70cff52721b88d052dd02c07b8d275fcdc870
Alsu Sharifullina <sharifullina1998@inbox.ru>
2025-02-21 18:08:06 +0300

    init: fix mistakes

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ themeToggler.addEventListener("change", () => {
     document.body.classList.toggle("dark-mode");
 });
 
+document.querySelectorAll('.header__item a').forEach(link => {
+    link.addEventListener('click', function() {
+        // Закрываем меню
+        document.querySelector('.menu_toggler').checked = false;
+    });
+});
